74865bb697f533c2dccddec0ec384413208ce9fc
Adeena Khan <145704470+adeenasif@users.noreply.github.com>
2025-10-02 00:05:11 -0700

    Create Q5.asm

---
+++ Lab5/Q5.asm
@@ -0,0 +1,23 @@
+INCLUDE Irvine32.inc
+
+.data
+    arrayD DWORD 1000h,2000h,3000h,4000h
+
+.code
+main PROC
+    ; PART a
+    mov esi, 1
+    mov eax, arrayD[esi * TYPE arrayD]  ; eax = 2000h
+    call WriteHex     
+    call Crlf
+    
+    ; PART b
+    mov esi, 3
+    mov ebx, arrayD[esi * TYPE arrayD]  ; ebx = 4000h
+    call dumpregs
+
+    ; PART c
+    TYPE operator is used to automatically calculate the correct offset
+exit
+main ENDP
+END main
